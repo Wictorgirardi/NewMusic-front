87f9652646aaaa465b381f34d672c63d5a55bb27
Wictor Girardi <wictorgirardi@Wictors-MacBook-Pro.local>
2025-01-10 15:27:38 -0300

    Finished Aplication Docker

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,6 +25,7 @@ export default function LoginPage() {
     try {
       loginMutation.mutate({ email, password });
     } catch (error) {
+      console.log(error);
       toast({ title: "Erro", description: ERROR_MESSAGE_LOGIN });
     }
   };
